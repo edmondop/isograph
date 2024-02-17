ad435a412d72ae160763c06950bf0d4bc9f32bb7
Edmondo Porcu <edmondo.porcu@gmail.com>
2024-02-17 22:41:30 +0000

    wip wop

---
+++ crates/swc_isograph_plugin/src/lib.rs
@@ -0,0 +1,62 @@
+use std::path::PathBuf;
+
+use serde_json::Value;
+use swc_common::{plugin::metadata::TransformPluginMetadataContextKind, FileName};
+use swc_core::{
+    ecma::{ast::Program, visit::FoldWith},
+    plugin::{plugin_transform, proxies::TransformPluginProgramMetadata},
+};
+
+use swc_isograph:::Config
+
+#[plugin_transform]
+fn isograph_plugin_transform(
+    program: Program,
+    metadata: TransformPluginProgramMetadata,
+) -> Program {
+    let filename = if let Some(filename) =
+        metadata.get_context(&TransformPluginMetadataContextKind::Filename)
+    {
+        FileName::Real(PathBuf::from(filename))
+    } else {
+        FileName::Anon
+    };
+
+    let plugin_config: Value = serde_json::from_str(
+        &metadata
+            .get_transform_plugin_config()
+            .expect("failed to get plugin config for isograph"),
+    )
+    .expect("Should provide plugin config");
+
+    let artifact_directory = plugin_config["artifact_directory"]
+        .as_str()
+        .map(PathBuf::from);
+    let project_root = plugin_config["project_root"].as_str().map(PathBuf::from);
+
+    // let language = plugin_config["language"]
+    //     .as_str()
+    //     .map_or(RelayLanguageConfig::TypeScript, |v| v.try_into().unwrap());
+    // let output_file_extension = plugin_config["outputFileExtension"]
+    //     .as_str()
+    //     .map_or(OutputFileExtension::Undefined, |v| v.try_into().unwrap());
+    // let eager_es_modules = plugin_config["eagerEsModules"]
+    //     .as_bool()
+    //     .unwrap_or_default();
+
+    let config = Config {
+        artifact_directory,
+        project_root,
+    };
+
+    // let mut relay = relay(
+    //     &config,
+    //     filename,
+    //     root_dir,
+    //     None,
+    //     Some(metadata.unresolved_mark),
+    // );
+
+    // program.fold_with(&mut relay)
+    todo!("not implemented yet")
+}
--- crates/swc_isograph_plugin/transform/src/lib.rs
+++ crates/swc_isograph_plugin/transform/src/lib.rs
@@ -11,61 +11,6 @@ use swc_ecma_ast::*;
 use swc_ecma_utils::{quote_ident, ExprFactory};
 use swc_ecma_visit::{Fold, FoldWith};
 
-#[derive(Copy, Clone, Debug, Deserialize)]
-#[serde(rename_all = "lowercase")]
-pub enum RelayLanguageConfig {
-    TypeScript,
-    JavaScript,
-    Flow,
-}
-
-#[derive(Copy, Clone, Debug, Deserialize)]
-#[serde(rename_all = "lowercase")]
-pub enum OutputFileExtension {
-    TypeScript,
-    JavaScript,
-    Undefined,
-}
-
-impl<'a> TryFrom<&'a str> for RelayLanguageConfig {
-    type Error = String;
-
-    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        match value {
-            "flow" => Ok(Self::Flow),
-            "typescript" => Ok(Self::TypeScript),
-            _ => Err(format!("Unexpected language config value '{value}'")),
-        }
-    }
-}
-
-impl<'a> TryFrom<&'a str> for OutputFileExtension {
-    type Error = String;
-
-    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
-        match value {
-            "ts" => Ok(Self::TypeScript),
-            "js" => Ok(Self::JavaScript),
-            _ => Err(format!(
-                "Unexpected output file extension value '{}'",
-                value
-            )),
-        }
-    }
-}
-
-impl Default for RelayLanguageConfig {
-    fn default() -> Self {
-        Self::Flow
-    }
-}
-
-impl Default for OutputFileExtension {
-    fn default() -> Self {
-        Self::Undefined
-    }
-}
-
 #[derive(Debug, Clone)]
 struct RelayImport {
     path: JsWord,
@@ -102,59 +47,114 @@ struct Isograph<'a> {
     file_name: FileName,
     config: &'a Config,
     // imports: Vec<RelayImport>,
-    // unresolved_mark: Option<Mark>,
+    unresolved_mark: Option<Mark>,
 }
 
 #[derive(Deserialize, Debug, Default, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Config {
     pub artifact_directory: Option<PathBuf>,
-
     pub root_directory: Option<PathBuf>,
-    // #[serde(default)]
-    // pub language: RelayLanguageConfig,
-    // #[serde(default)]
-    // pub eager_es_modules: bool,
-    // #[serde(default)]
-    // pub output_file_extension: OutputFileExtension,
 }
 
-fn pull_first_operation_name_from_tpl(tpl: &TaggedTpl) -> Option<String> {
-    tpl.tpl.quasis.iter().find_map(|quasis| {
-        static OPERATION_REGEX: Lazy<Regex> =
-            Lazy::new(|| Regex::new(r"(fragment|mutation|query|subscription) (\w+)").unwrap());
-
-        let capture_group = OPERATION_REGEX.captures_iter(&quasis.raw).next();
-
-        capture_group.map(|capture_group| capture_group[2].to_string())
-    })
+struct IsographEntrypoint {
+    pub field_type: String,
+    pub field_name: String,
+    pub artifact_type: String,
 }
 
-fn build_require_expr_from_path(path: &str, mark: Option<Mark>) -> Expr {
-    Expr::Call(CallExpr {
-        span: Default::default(),
-        callee: quote_ident!(
-            mark.map(|m| DUMMY_SP.apply_mark(m)).unwrap_or(DUMMY_SP),
-            "require"
-        )
-        .as_callee(),
-        args: vec![Lit::Str(Str {
+impl IsographEntrypoint {
+    fn build_require_expr_from_path(path: &str, mark: Option<Mark>) -> Expr {
+        Expr::Call(CallExpr {
             span: Default::default(),
-            value: JsWord::from(path),
-            raw: None,
+            callee: quote_ident!(
+                mark.map(|m| DUMMY_SP.apply_mark(m)).unwrap_or(DUMMY_SP),
+                "require"
+            )
+            .as_callee(),
+            args: vec![Lit::Str(Str {
+                span: Default::default(),
+                value: JsWord::from(path),
+                raw: None,
+            })
+            .as_arg()],
+            type_args: None,
         })
-        .as_arg()],
-        type_args: None,
-    })
+    }
+    pub fn to_import_expr(self, config: &Config, unresolved_mark: Option<Mark>) -> Option<Expr> {
+        // const filename = path.state.filename;
+        // const folder = pathModule.dirname(filename);
+        // const cwd = path.state.cwd;
+        // const artifactDirectory = pathModule.join(
+        //   cwd,
+        //   config.artifact_directory ?? config.project_root,
+        // );
+
+        // const fileToArtifactDir = pathModule.relative(folder, artifactDirectory);
+        // const artifactDirToArtifact = `/__isograph/${type}/${field}/${artifactType}.ts`;
+        // let fileToArtifact = pathModule.join(
+        //   fileToArtifactDir,
+        //   artifactDirToArtifact,
+        // );
+        panic!("todo")
+    }
+
+    fn path_for_artifact(
+        &self,
+        real_file_name: &Path,
+        config: &Config,
+    ) -> Result<PathBuf, BuildRequirePathError> {
+        let filename = format!("/__isograph/{}/{}/{}.ts", self.field_type, self.field_name, self.artifact_type)
+
+        if let Some(artifact_directory) = config.artifact_directory {
+            Ok(root_dir.join(artifact_directory).join(filename))
+        }else {
+            Ok(real_file_name
+                .parent()
+                .unwrap()
+                .join("__generated__")
+                .join(filename))
+        }
+    }
+
+    // fn build_require_path(
+    //     &mut self,
+    //     operation_name: &str,
+    // ) -> Result<PathBuf, BuildRequirePathError> {
+    //     match &self.file_name {
+    //         FileName::Real(real_file_name) => {
+    //             self.path_for_artifact(real_file_name, operation_name)
+    //         }
+    //         _ => Err(BuildRequirePathError::FileNameNotReal),
+    //     }
+    // }
+}
+static OPERATION_REGEX: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"\\s*(entrypoint|field)\\s*([^\\.\\s]+)\\.([^\\s\\(]+)").unwrap());
+
+fn parse_iso_call_arg_into_type(expr_or_spread: &ExprOrSpread) -> Option<IsographEntrypoint> {
+    match *expr_or_spread.expr {
+        Expr::Lit(Lit::Str(iso_entrypoint)) => {
+            let capture_group = OPERATION_REGEX
+                .captures_iter(&iso_entrypoint.value.as_str())
+                .next();
+
+            capture_group.map(|capture_group| IsographEntrypoint {
+                field_type: capture_group[2].to_string(),
+                field_name: capture_group[3].to_string(),
+                artifact_type: "entrypoint".to_string(),
+            })
+        }
+        _ => panic!("iso function can only be called with a literal argument"),
+    }
 }
 
 impl<'a> Fold for Isograph<'a> {
     fn fold_expr(&mut self, expr: Expr) -> Expr {
         let expr = expr.fold_children_with(self);
-
         match &expr {
-            Expr::TaggedTpl(tpl) => {
-                if let Some(built_expr) = self.compile_iso_statement(tpl) {
+            Expr::Call(call_expr) => {
+                if let Some(built_expr) = self.compile_call_statement(call_expr) {
                     built_expr
                 } else {
                     expr
@@ -162,19 +162,6 @@ impl<'a> Fold for Isograph<'a> {
             }
             _ => expr,
         }
-    }
-
-    fn fold_module_items(&mut self, items: Vec<ModuleItem>) -> Vec<ModuleItem> {
-        let items = items
-            .into_iter()
-            .map(|item| item.fold_children_with(self))
-            .collect::<Vec<_>>();
-
-        self.imports
-            .iter()
-            .map(|import| import.as_module_item())
-            .chain(items.into_iter())
-            .collect()
     }
 }
 
@@ -189,131 +176,37 @@ enum BuildRequirePathError {
     ArtifactDirectoryExpected { file_name: String },
 }
 
-impl<'a> Relay<'a> {
-    fn path_for_artifact(
-        &self,
-        real_file_name: &Path,
-        definition_name: &str,
-    ) -> Result<PathBuf, BuildRequirePathError> {
-        let filename = match &self.config.output_file_extension {
-            OutputFileExtension::JavaScript => format!("{}.graphql.js", definition_name),
-            OutputFileExtension::TypeScript => format!("{}.graphql.ts", definition_name),
-            OutputFileExtension::Undefined => match &self.config.language {
-                RelayLanguageConfig::Flow => format!("{}.graphql.js", definition_name),
-                RelayLanguageConfig::TypeScript => format!("{}.graphql.ts", definition_name),
-                RelayLanguageConfig::JavaScript => format!("{}.graphql.js", definition_name),
-            },
-        };
-
-        if let Some(artifact_directory) = &self.config.artifact_directory {
-            Ok(self.root_dir.join(artifact_directory).join(filename))
-        } else if self
-            .pages_dir
-            .as_ref()
-            .map_or(false, |pages_dir| real_file_name.starts_with(pages_dir))
-        {
-            Err(BuildRequirePathError::ArtifactDirectoryExpected {
-                file_name: real_file_name.display().to_string(),
-            })
-        } else {
-            Ok(real_file_name
-                .parent()
-                .unwrap()
-                .join("__generated__")
-                .join(filename))
-        }
-    }
-
-    fn build_require_path(
-        &mut self,
-        operation_name: &str,
-    ) -> Result<PathBuf, BuildRequirePathError> {
-        match &self.file_name {
-            FileName::Real(real_file_name) => {
-                self.path_for_artifact(real_file_name, operation_name)
-            }
-            _ => Err(BuildRequirePathError::FileNameNotReal),
-        }
-    }
-
-    fn build_call_expr_from_tpl(&mut self, tpl: &TaggedTpl) -> Option<Expr> {
-        if let Expr::Ident(ident) = &*tpl.tag {
-            if &*ident.sym != "graphql" {
-                return None;
-            }
-        }
-
-        let operation_name = pull_first_operation_name_from_tpl(tpl);
-
-        match operation_name {
+impl<'a> Isograph<'a> {
+    fn compile_iso_call_statement(&mut self, call_expr: &CallExpr) -> Option<Expr> {
+        let entrypoint = parse_iso_call_arg_into_type(&call_expr.args[0]);
+        match entrypoint {
+            Some(entrypoint) => entrypoint.to_import_expr(self.config, self.unresolved_mark),
             None => None,
-            Some(operation_name) => match self.build_require_path(&operation_name) {
-                Ok(final_path) => {
-                    let final_path = final_path.to_string_lossy();
-
-                    #[cfg(target_os = "windows")]
-                    let final_path = final_path.replace("\\", "/");
-
-                    let ident_name: JsWord =
-                        unique_ident_name_from_operation_name(&operation_name).into();
-
-                    if self.config.eager_es_modules {
-                        self.imports.push(RelayImport {
-                            path: final_path.into(),
-                            item: ident_name.clone(),
-                            unresolved_mark: self.unresolved_mark,
-                        });
-                        let operation_ident = Ident {
-                            span: self
-                                .unresolved_mark
-                                .map(|m| DUMMY_SP.apply_mark(m))
-                                .unwrap_or(Default::default()),
-                            sym: ident_name,
-                            optional: false,
-                        };
-                        Some(Expr::Ident(operation_ident))
-                    } else {
-                        Some(build_require_expr_from_path(
-                            &final_path,
-                            self.unresolved_mark,
-                        ))
-                    }
-                }
-                Err(_err) => {
-                    // let base_error = "Could not transform GraphQL template to a Relay import.";
-                    // let error_message = match err {
-                    //     BuildRequirePathError::FileNameNotReal => {
-                    //         "Source file was not a real file.".to_string()
-                    //     }
-                    // };
-
-                    // HANDLER.with(|handler| {
-                    //     handler.span_err(
-                    //         tpl.span,
-                    //         format!("{} {}", base_error, error_message).as_str(),
-                    //     );
-                    // });
-
-                    None
-                }
-            },
         }
+    }
+
+    fn compile_call_statement(&mut self, call_expr: &CallExpr) -> Option<Expr> {
+        // Maybe move to visitMut?
+        let sub_expr = call_expr.callee.clone().expr();
+        if let Some(boxed_expr) = sub_expr {
+            if let Expr::Ident(ident) = *boxed_expr {
+                if ident.sym == "iso" {
+                    return self.compile_iso_call_statement(call_expr);
+                }
+            }
+        }
+        None
     }
 }
 
 pub fn isograph(
     config: &Config,
     file_name: FileName,
-    // root_dir: PathBuf,
-    // pages_dir: Option<PathBuf>,
-    // unresolved_mark: Option<Mark>,
+    unresolved_mark: Option<Mark>,
 ) -> impl Fold + '_ {
     Isograph {
-        root_dir,
         file_name,
         config,
-        pages_dir,
-        imports: vec![],
         unresolved_mark,
     }
 }
